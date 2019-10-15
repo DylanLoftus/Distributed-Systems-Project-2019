38693d6c27b105ecd66179f1604f31e2bcfde42a
DylanLoftus <dylanloftus@hotmail.com>
2019-10-15 16:30:41 +0100

    Update PasswordServiceImpl.java

--- grpc-password-service/src/main/java/ie/gmit/ds/PasswordServiceImpl.java
+++ grpc-password-service/src/main/java/ie/gmit/ds/PasswordServiceImpl.java
@@ -1,12 +1,16 @@
 package ie.gmit.ds;
 
+import java.security.SecureRandom;
+import java.util.Random;
+
 import io.grpc.stub.StreamObserver;
 
 public class PasswordServiceImpl extends PasswordServiceGrpc.PasswordServiceImplBase  {
+	
+	private static final Random RANDOM = new SecureRandom();
 
 	@Override
 	public void hash(PasswordCreateRequest request, StreamObserver<PasswordCreateResponse> responseObserver) {
-		
 		
 	}
 
@@ -15,7 +19,36 @@ public class PasswordServiceImpl extends PasswordServiceGrpc.PasswordServiceImpl
 		
 		
 	}
-
 	
+	public static byte[] makeSalt() {
+        byte[] salt = new byte[32];
+        RANDOM.nextBytes(salt);
+        return salt;
+    }
 	
+	public static String makeRandomPassword(int length) {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int c = RANDOM.nextInt(62);
+            if (c <= 9) {
+                sb.append(String.valueOf(c));
+            } else if (c < 36) {
+                sb.append((char) ('a' + c - 10));
+            } else {
+                sb.append((char) ('A' + c - 36));
+            }
+        }
+        return sb.toString();
+    }
+	
+	public static void main(String[] args) {
+		String randomPass;
+		
+		PasswordServiceImpl ps = new PasswordServiceImpl();
+		
+		randomPass = PasswordServiceImpl.makeRandomPassword(5);
+		
+		ps.hash(randomPass, responseObserver);
+		
+	}
 }
