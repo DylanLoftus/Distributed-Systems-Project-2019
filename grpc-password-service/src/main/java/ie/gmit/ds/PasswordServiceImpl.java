afd87af55ef649f7c6b15967ab4a0c21e5e2dbe8
DylanLoftus <dylanloftus@hotmail.com>
2019-10-15 15:36:39 +0100

    Update PasswordServiceImpl.java

--- grpc-password-service/src/main/java/ie/gmit/ds/PasswordServiceImpl.java
+++ grpc-password-service/src/main/java/ie/gmit/ds/PasswordServiceImpl.java
@@ -1,6 +1,18 @@
 package ie.gmit.ds;
 
+import io.grpc.stub.StreamObserver;
+
 public class PasswordServiceImpl extends PasswordServiceGrpc.PasswordServiceImplBase  {
+
+	@Override
+	public void hash(PasswordCreateRequest request, StreamObserver<PasswordCreateResponse> responseObserver) {
+		super.hash(request, responseObserver);
+	}
+
+	@Override
+	public void validate(PasswordValidateRequest request, StreamObserver<PasswordValidateResponse> responseObserver) {
+		super.validate(request, responseObserver);
+	}
 
 	
 	
