8e1895859be8f594f37183c86cf5f6c8244a2adc
DylanLoftus <dylanloftus@hotmail.com>
2019-10-21 14:02:06 +0100

    Create Password Implementation

--- grpc-password-service/src/main/java/ie/gmit/ds/PasswordClient.java
+++ grpc-password-service/src/main/java/ie/gmit/ds/PasswordClient.java
@@ -1,9 +1,85 @@
 package ie.gmit.ds;
 
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.google.protobuf.BoolValue;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import io.grpc.stub.StreamObserver;
+
 public class PasswordClient {
 
-	public static void main(String[] args) {
+	private static final Logger logger =
+            Logger.getLogger(PasswordClient.class.getName());
+    private final ManagedChannel channel;
+    private final PasswordServiceGrpc.PasswordServiceStub asyncPasswordService;
+    private final PasswordServiceGrpc.PasswordServiceBlockingStub syncPasswordService;
 
+    public PasswordClient(String host, int port) {
+        channel = ManagedChannelBuilder
+                .forAddress(host, port)
+                .usePlaintext()
+                .build();
+        syncPasswordService = PasswordServiceGrpc.newBlockingStub(channel);
+        asyncPasswordService = PasswordServiceGrpc.newStub(channel);
+    }
+    
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+    
+    
+    
+    private void makePassword(String password, int userID) {
+		StreamObserver<PasswordCreateResponse> responseObserver = new StreamObserver<PasswordCreateResponse>() {
+
+			@Override
+			public void onNext(PasswordCreateResponse value) {
+				System.out.println(value.getHashedPassword().toByteArray());
+				System.out.println(value.getUserId());
+				System.out.println(value.getSalt().toByteArray());
+			}
+
+			@Override
+			public void onError(Throwable t) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onCompleted() {
+				//validatePassword(value.get)
+			}
+		};
+		
+		PasswordCreateRequest request = PasswordCreateRequest.newBuilder().setUserId(userID).setPassword(password).build();
+		asyncPasswordService.hash(request, responseObserver);
+		
 	}
+	
+    public static void main(String[] args) throws Exception {
+    	
+    	Scanner console = new Scanner(System.in);  
+    	
+    	PasswordClient client = new PasswordClient("localhost", 50551);
+        
+    	System.out.println("Enter password: ");
+    	String password = console.next();
+    	
+    	System.out.println("Enter user ID: ");
+    	int userId = console.nextInt();
+    	
+        try {
+            client.makePassword(password, userId);
+        } finally {
+            // Don't stop process, keep alive to receive async response
+            Thread.currentThread().join();
+        }
+    }
 
 }
--- grpc-password-service/src/main/java/ie/gmit/ds/PasswordServiceImpl.java
+++ grpc-password-service/src/main/java/ie/gmit/ds/PasswordServiceImpl.java
@@ -1,27 +1,17 @@
 package ie.gmit.ds;
 
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.spec.InvalidKeySpecException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
 import java.util.logging.Logger;
 
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
-
 import com.google.protobuf.BoolValue;
+import com.google.protobuf.ByteString;
 
 import io.grpc.stub.StreamObserver;
 
 public class PasswordServiceImpl extends PasswordServiceGrpc.PasswordServiceImplBase  {
 	
-	private static final int ITERATIONS = 10000;
-	private static final int KEY_LENGTH = 256;
+	
 	private static final Logger logger =
             Logger.getLogger(PasswordServiceImpl.class.getName());
-	private static final Random RANDOM = new SecureRandom();
 	
 	public PasswordServiceImpl() {
 		
@@ -29,65 +19,31 @@ public class PasswordServiceImpl extends PasswordServiceGrpc.PasswordServiceImpl
 
 	@Override
 	public void hash(PasswordCreateRequest request, StreamObserver<PasswordCreateResponse> responseObserver) {
-		String password = PasswordCreateRequest.newBuilder().getPassword();
-		char[] passwordChar = password.toCharArray();
-		hashPass(passwordChar, makeSalt());
+		byte[] salt = Passwords.getNextSalt();
+		String password = request.getPassword();
+		byte[] hashedPassword = Passwords.hash(password.toCharArray(), salt);
+		
+		ByteString bsSalt = ByteString.copyFrom(salt);
+		ByteString bsHashPassword = ByteString.copyFrom(hashedPassword);
+		
+		int userID = request.getUserId();
+		
+		PasswordCreateResponse reply = PasswordCreateResponse.newBuilder().setHashedPassword(bsHashPassword).setUserId(userID).setSalt(bsSalt).build();
+		responseObserver.onNext(reply);
         responseObserver.onCompleted();
-    
 	}
-	
-	
 	
 	@Override
 	public void validate(PasswordValidateRequest request, StreamObserver<BoolValue> responseObserver) {
-		super.validate(request, responseObserver);
-	}
-
-	public static byte[] makeSalt() {
-        byte[] salt = new byte[32];
-        RANDOM.nextBytes(salt);
-        return salt;
-    }
-	
-	public static String makeRandomPassword(int length) {
-        StringBuilder sb = new StringBuilder(length);
-        for (int i = 0; i < length; i++) {
-            int c = RANDOM.nextInt(62);
-            if (c <= 9) {
-                sb.append(String.valueOf(c));
-            } else if (c < 36) {
-                sb.append((char) ('a' + c - 10));
-            } else {
-                sb.append((char) ('A' + c - 36));
-            }
+		try {
+            
+            //logger.info("Passwords are correct");
+            responseObserver.onNext(BoolValue.newBuilder().setValue(true).build());
+        } catch (RuntimeException ex) {
+            responseObserver.onNext(BoolValue.newBuilder().setValue(false).build());
         }
-        return sb.toString();
-    }
-	
-	public static byte[] hashPass(char[] password, byte[] salt) {
-        PBEKeySpec spec = new PBEKeySpec(password, salt, ITERATIONS, KEY_LENGTH);
-        try {
-            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-            return skf.generateSecret(spec).getEncoded();
-        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-            throw new AssertionError("Error while hashing a password: " + e.getMessage(), e);
-        } finally {
-            spec.clearPassword();
-        }
-    }
-	
-	public static void main(String[] args) {
-		String randomPass = makeRandomPassword(10);
-        byte[] salt = makeSalt();
-        char[] passArray = randomPass.toCharArray();
-        
-        byte[] pwdHash = hashPass(passArray, salt);
-        
-        System.out.println(randomPass);
-        
-        System.out.println(Arrays.toString(salt));
-        
-        System.out.println(Arrays.toString(pwdHash));
+        responseObserver.onCompleted();
 	}
+	
 	
 }
